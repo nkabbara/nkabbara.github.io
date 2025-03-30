cd607f82abf7ae78e14c40584049ef86d2c06465
Nash Kabbara <nash.kabbara@kape.com>
2025-03-30 15:42:03 -0300

    finish sorting

--- assets/javascript/sort.js
+++ assets/javascript/sort.js
@@ -1,10 +1,3 @@
-// Specs for sorting:
-// 1. When page loads, articles are sorted by last created.
-// 2. Sorting is only in reverse chronological order.
-// 3. Mark is over the sorted attribute only.
-// 4. Emjory showing reverse order is next to attribute we're dorting by.
-// 5. Sorting happens on page by JS, no reloads.
-
 function sortListByDate(dateAttribute) {
   const postTitleList = document.getElementById("post-titles");
   const postTitleItems = Array.from(postTitleList.getElementsByTagName("li"));
@@ -25,8 +18,9 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
     sortListByDate("created");
-    // Remove arrow from sort-by-updated
-    this.textContent = "⬇️ " + this.textContent;
+    this.classList.add("active");
+    const byUpdated = document.getElementById("sort-by-updated");
+    byUpdated.classList.remove("active");
   });
 
 document
@@ -34,6 +28,7 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
     sortListByDate("updated");
-    // Remove arrow from sort-by-created
-    this.textContent = "⬇️ " + this.textContent;
+    this.classList.add("active");
+    const byCreated = document.getElementById("sort-by-created");
+    byCreated.classList.remove("active");
   });
